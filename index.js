f6a4b89a7dee9ee4cf9807384e383acd757ad3a7
Vladyslav Nesterenko <easterenko@gmail.com>
2017-11-15 18:07:41 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function perform () {
+  var args = Array.from(arguments);
+  var callback = args.pop();
+  var param = callback.apply(null, args);
+ 
+  this.then = function (callback) {
+    return perform(param, callback);
+  }
+  
+  return this;
+}
+
+perform(null, function() {
+  var param = 1;
+  console.log(param); // 1
+  return param;
+})
+  .then(function(param) { // param === 1
+    console.log(++param); // 2
+    return param;
+  })
+  .then(function(param) { // param === 2
+    console.log(++param); // 3
+    return param;
+});
